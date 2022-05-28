9d9647f6bafbfb585b24efc577a4dc7146940813
manishtmtmt <manisht.mt.mt@gmail.com>
2022-05-28 09:01:20 +0530

    sort and filtering part done

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,15 +4,32 @@ import TableItem from "./TableItem";
 
 const Table = ({ data, setData }) => {
   const [page, setPage] = useState(1);
+  const [department, setDepartment] = useState()
+  const [order, setOrder] = useState()
   useEffect(() => {
-    fetch(`http://localhost:3040/data?_page=${page}&_limit=5`)
+    fetch(`http://localhost:3040/data?_page=${page}&_limit=5&department=${department}&_sort=salary&_order=${order}`)
       .then((r) => r.json())
       .then((d) => {
         setData(d);
       });
-  }, [page]);
+  }, [page,department,order]);
   return (
     <div className={styles.table}>
+        <div>
+            <select onChange={(e)=>setOrder(e.target.value)}>
+                <option value="">Sort by salary</option>
+                <option value="asc">Low to High</option>
+                <option value="desc">High to Low</option>
+            </select>
+            <select onChange={(e)=>setDepartment(e.target.value)}>
+                <option value="">Filter by department</option>
+                <option value="Manager">Manager</option>
+                <option value="Executive Officer">Executive Officer</option>
+                <option value="Officer">Officer</option>
+                <option value="Junior Officer">Junior Officer</option>
+                <option value="Assistant Officer">Assistant Officer</option>
+            </select>
+        </div>
       <table>
         <thead>
           <tr>
